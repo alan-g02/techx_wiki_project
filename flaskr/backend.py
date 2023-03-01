c4fbb2bec0afa3ddf83570bcbc5f57706644974f
Peasare <peasareakwesi@techexchange.in>
2023-03-01 22:15:25 +0000

    Added backend for upload and getting all page names together with test

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,17 +1,51 @@
 # TODO(Project 1): Implement Backend according to the requirements.
+from google.cloud import storage
 class Backend:
 
     def __init__(self):
-        pass
+        self.list_page_names = []
         
     def get_wiki_page(self, name):
         pass
 
-    def get_all_page_names(self):
-        pass
+    def get_all_page_names(self, bucket_name, blob_name):
+        """Write and read a blob from GCS using file-like IO"""
+        # The ID of your GCS bucket
+        # bucket_name = "your-bucket-name"
 
-    def upload(self):
-        pass
+        # The ID of your new GCS object
+        # blob_name = "storage-object-name"
+
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+
+        with blob.open("r") as f:
+            for name in f:
+                self.list_page_names.append(name)  
+
+        return self.list_page_names
+
+
+
+    def upload(self, bucket_name, source_file_name, destination_blob_name):
+        """Uploads a file to the bucket."""
+        # The ID of your GCS bucket
+        # bucket_name = "your-bucket-name"
+        # The path to your file to upload
+        # source_file_name = "local/path/to/file"
+        # The ID of your GCS object
+        # destination_blob_name = "storage-object-name"
+
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+
+        blob.upload_from_filename(source_file_name)
+
+        return 
+
+
 
     def sign_up(self):
         pass
--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -1,3 +1,40 @@
+import pytest
 from flaskr.backend import Backend
-
+from unittest.mock import MagicMock,patch
 # TODO(Project 1): Write tests for Backend methods.
+
+@patch("google.cloud.storage.Client")
+def test_upload(mock_storage):
+    mock_client = MagicMock()
+    mock_storage.return_value = mock_client
+
+    bucket = MagicMock()
+    mock_client.bucket.return_value = bucket
+
+    blob = MagicMock()
+    bucket.blob.return_value = blob    
+
+    backend = Backend()
+    backend.upload("wiki", "desktop","gcb")
+    mock_client.bucket.assert_called_with("wiki")
+    bucket.blob.assert_called_with("gcb")
+
+    blob.upload_from_filename.assert_called_with("desktop")
+
+
+@patch("google.cloud.storage.Client")
+def test_get_all_page_names(mock_storage):
+    mock_client = MagicMock()
+    mock_storage.return_value = mock_client
+
+    bucket = MagicMock()
+    mock_client.bucket.return_value = bucket
+
+    blob = MagicMock()
+    bucket.blob.return_value = blob    
+
+    backend = Backend()
+    backend.get_all_page_names("wiki", "pages")
+    
+    mock_client.bucket.assert_called_with("wiki")
+    bucket.blob.assert_called_with("pages")

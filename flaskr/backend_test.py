f8a6fd453c91f4facbab7482790d837e1bbab8d8
Alan Gonzalez <alan_g@techexchange.in>
2023-03-06 02:55:20 +0000

    Modified backend_test.py for multiple mock class uses.

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -1,20 +1,21 @@
-import pytest
 from flaskr.backend import Backend
 from unittest.mock import MagicMock,patch
-# TODO(Project 1): Write tests for Backend methods.
+
+mock_client = MagicMock()
+bucket = MagicMock()
+blob = MagicMock()
+backend = Backend()
 
 @patch("google.cloud.storage.Client")
 def test_upload(mock_storage):
-    mock_client = MagicMock()
     mock_storage.return_value = mock_client
 
-    bucket = MagicMock()
     mock_client.bucket.return_value = bucket
 
-    blob = MagicMock()
     bucket.blob.return_value = blob    
 
     backend = Backend()
+    
     backend.upload("wiki", "desktop","gcb")
     mock_client.bucket.assert_called_with("wiki")
     bucket.blob.assert_called_with("gcb")
@@ -24,17 +25,12 @@ def test_upload(mock_storage):
 
 @patch("google.cloud.storage.Client")
 def test_get_all_page_names(mock_storage):
-    mock_client = MagicMock()
     mock_storage.return_value = mock_client
 
-    bucket = MagicMock()
     mock_client.bucket.return_value = bucket
 
-    blob = MagicMock()
     bucket.blob.return_value = blob    
 
-    backend = Backend()
     backend.get_all_page_names("wiki", "pages")
-    
     mock_client.bucket.assert_called_with("wiki")
     bucket.blob.assert_called_with("pages")

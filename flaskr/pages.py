48662ab0105dee0a9769b51a4690c5e989056dd0
Alan Gonzalez <alan_g@techexchange.in>
2023-03-06 03:01:46 +0000

    Cleaned up pages.py of unnecesary comments.

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -4,13 +4,8 @@ from flaskr import backend
 backend = backend.Backend()
 
 def make_endpoints(app):
-
-    # Flask uses the "app.route" decorator to call methods when users
-    # go to a specific route on the project's website.
     @app.route("/")
     def home():
-        # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
-        # to render main.html on the home page.
         return render_template('home.html')
     
     @app.route("/about")

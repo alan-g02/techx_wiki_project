3e1c5dc941e64ba241fb31a2f74838516526f4a3
Alan Gonzalez <alan_g@techexchange.in>
2023-03-11 03:38:57 +0000

    Prepared sign_in for mocks and created tests for it

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -157,9 +157,7 @@ def test_get_image_fail(mock_storage):
 
     '''
     class MockBytes:
-        '''Mocks the class of BytesIO
-
-        Mocks the class of BytesIO with similar needed methods.
+        '''Mocks the class of BytesIO with similar needed methods.
 
         Attributes:
             data: saves data when initializing the object
@@ -167,9 +165,7 @@ def test_get_image_fail(mock_storage):
         def __init__(self,data):
             self.data = data
         def read(self):
-            '''
-            Returns the attribute data in the class
-            '''
+            # Returns the attribute data in the class
             return self.data
 
     # Creates Magic Mocks
@@ -198,6 +194,139 @@ def test_get_image_fail(mock_storage):
 
     # Since the blob does not exist, it returns None
     assert results == None
+
+@patch("google.cloud.storage.Client")
+def test_sign_in_found_match(mock_storage):
+    '''Tests sign in(), expects to find user and the password matches
+
+    Assumes the blob exists, open returns the password in a string.
+    Path: User blob exists, password in blob and password match.
+    Removes dependencies: storage client, bucket, blob, and hashlib
+    '''
+    class Hash:
+        '''Mocks the hashlib.sha256 class with similar needed methods.
+
+        Attributes:
+            data: used to store a string
+        '''
+        def __init__(self,data):
+            self.data = data.decode()
+        
+        def hexdigest(self):
+            return self.data
+
+    # Creates Magic Mocks
+    my_client = MagicMock()
+    my_bucket = MagicMock()
+    my_blob = MagicMock()
+
+    backend = Backend()
+
+    # Establishes return values for mock operations
+    mock_storage = my_client
+    my_client.bucket.return_value = my_bucket
+    my_bucket.blob.return_value = my_blob
+    my_blob.exists.return_value = True # Makes the function believe that the blob exists
+
+    password = "password123" # Same as the input in the method
+
+    # Sets blob open value of read to password
+    my_blob.open = mock_open(read_data=password)
+
+    result = backend.sign_in("user123",
+                            "password123",
+                            mock_storage,
+                            Hash)
+    assert result == True
+
+@patch("google.cloud.storage.Client")
+def test_sign_in_found_nomatch(mock_storage):
+    '''Tests sign in(), expects to find user and the password does not match
+
+    Assumes the blob exists, open returns the password in a string.
+    Path: User blob exists, password in blob and password do not match.
+    Removes dependencies: storage client, bucket, blob, and hashlib
+    '''
+    class Hash:
+        '''Mocks the hashlib.sha256 class with similar needed methods.
+
+        Attributes:
+            data: used to store a string
+        '''
+        def __init__(self,data):
+            self.data = data.decode()
+        
+        def hexdigest(self):
+            return self.data
+
+    # Creates Magic Mocks
+    my_client = MagicMock()
+    my_bucket = MagicMock()
+    my_blob = MagicMock()
+
+    backend = Backend()
+
+    # Establishes return values for mock operations
+    mock_storage = my_client
+    my_client.bucket.return_value = my_bucket
+    my_bucket.blob.return_value = my_blob
+    my_blob.exists.return_value = True # Makes the function believe that the blob exists
+
+    password = "pass123" # Not the same as the input in the method
+
+    # Sets blob open value of read to password
+    my_blob.open = mock_open(read_data=password)
+
+    result = backend.sign_in("user123",
+                            "pass321",
+                            mock_storage,
+                            Hash)
+    assert result == False
+
+@patch("google.cloud.storage.Client")
+def test_sign_in_not_found(mock_storage):
+    '''Tests sign in(), does not find the user.
+
+    Assumes the blob exists, open returns the password in a string.
+    Path: User does not exist.
+    Does not matter if password and and input match, user does not exist.
+    Removes dependencies: storage client, bucket, blob, and hashlib
+    '''
+    class Hash:
+        '''Mocks the hashlib.sha256 class with similar needed methods.
+
+        Attributes:
+            data: used to store a string
+        '''
+        def __init__(self,data):
+            self.data = data.decode()
+        
+        def hexdigest(self):
+            return self.data
+
+    # Creates Magic Mocks
+    my_client = MagicMock()
+    my_bucket = MagicMock()
+    my_blob = MagicMock()
+
+    backend = Backend()
+
+    # Establishes return values for mock operations
+    mock_storage = my_client
+    my_client.bucket.return_value = my_bucket
+    my_bucket.blob.return_value = my_blob
+    my_blob.exists.return_value = False # Makes the function believe that the blob exists
+
+    password = "password123"
+
+    # Sets blob open value of read to password
+    my_blob.open = mock_open(read_data=password)
+
+    result = backend.sign_in("user123",
+                            "password123",
+                            mock_storage,
+                            Hash)
+    assert result == False
     
     
     
